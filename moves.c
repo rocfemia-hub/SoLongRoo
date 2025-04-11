b2c65b8399b26782d0bda16908472d053579d506
Your Name <you@example.com>
2025-04-12 01:48:23 +0200

    actualizando so_long

--- moves.c
+++ moves.c
@@ -6,7 +6,7 @@
 /*   By: roo <roo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:00:19 by roo               #+#    #+#             */
-/*   Updated: 2025/04/12 01:30:56 by roo              ###   ########.fr       */
+/*   Updated: 2025/04/12 01:47:59 by roo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,12 @@ int	press_keys(int keycode, t_mlx *var_mlx)
 	
 	next_row = var_mlx->var_map2->row_player;
 	next_col = var_mlx->var_map2->col_player;
+	next_tile = var_mlx->var_map2->map[next_row][next_col];
 	if (keycode == 119)
 	{
 		next_row--;
+		if (next_row < 0 || next_row >= var_mlx->var_map2->map_h || next_col < 0 || next_col >= var_mlx->var_map2->map_w)
+			return (0);		
 		next_tile = var_mlx->var_map2->map[next_row][next_col];
 		if (next_tile == '1')
 			return (0);
@@ -31,6 +34,7 @@ int	press_keys(int keycode, t_mlx *var_mlx)
 	else if (keycode == 97)
 	{
 		next_col--;
+		if (next_row < 0 || next_row >= var_mlx->var_map2->map_h || next_col < 0 || next_col >= var_mlx->var_map2->map_w)
 		next_tile = var_mlx->var_map2->map[next_row][next_col];
 		if (next_tile == '1')
 			return (0);
@@ -39,6 +43,7 @@ int	press_keys(int keycode, t_mlx *var_mlx)
 	else if (keycode == 115)
 	{
 		next_row++;
+		if (next_row < 0 || next_row >= var_mlx->var_map2->map_h || next_col < 0 || next_col >= var_mlx->var_map2->map_w)
 		next_tile = var_mlx->var_map2->map[next_row][next_col];
 		if (next_tile == '1')
 			return (0);
@@ -47,6 +52,7 @@ int	press_keys(int keycode, t_mlx *var_mlx)
 	else if (keycode == 100)
 	{
 		next_col++;
+		if (next_row < 0 || next_row >= var_mlx->var_map2->map_h || next_col < 0 || next_col >= var_mlx->var_map2->map_w)
 		next_tile = var_mlx->var_map2->map[next_row][next_col];
 		if (next_tile == '1')
 			return (0);
